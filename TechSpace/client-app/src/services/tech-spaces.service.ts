f41ec7621859dfc7add2e966dcd1f78e550add85
LukeBillo <luke.o.billington@gmail.com>
2020-08-19 16:57:03 +0100

    Started adding HTTP service layer

---
+++ TechSpace/client-app/src/services/tech-spaces.service.ts
@@ -0,0 +1,40 @@
+﻿import Axios, {AxiosInstance} from "axios";
+
+export class TechnologySpace {
+    
+}
+
+export class TechSpacesService {
+    private readonly axiosClient: AxiosInstance;
+    
+    constructor() {
+        this.axiosClient = Axios.create({
+            baseURL: 'https://localhost:5001/api/spaces',
+            responseType: 'json',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    async getAll(): Promise<Array<TechnologySpace>> {
+        const response = await this.axiosClient.get<Array<TechnologySpace>>('all');
+        if (response.status !== 200) {
+            console.error(`Error from TechSpacesService.getAll(). Received HTTP status ${response.status}: ${response.statusText}`);
+        }
+        
+        return response.data;
+    }
+
+    async get(name: string): Promise<TechnologySpace> {
+        const response = await this.axiosClient.get<TechnologySpace>(name);
+        if (response.status !== 200) {
+            console.error(`Error from TechSpacesService.get(${name}). Received HTTP status ${response.status}: ${response.statusText}`);
+        }
+
+        return response.data;
+    }
+}
+
+export const TechSpacesClient = new TechSpacesService();
+
